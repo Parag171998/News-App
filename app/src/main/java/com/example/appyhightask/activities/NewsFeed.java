e6d3d7b4ec971df6c49f6cfbc9fc4fdc86ba00cc
PARAG <paragranerab1998@gmail.com>
2020-08-19 11:36:03 +0530

    Added Admob Native ad and firebase remote config.

--- app/src/main/java/com/example/appyhightask/activities/NewsFeed.java
+++ app/src/main/java/com/example/appyhightask/activities/NewsFeed.java
@@ -1,5 +1,6 @@
 package com.example.appyhightask.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
@@ -9,15 +10,29 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.webkit.DownloadListener;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import com.example.appyhightask.R;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
 import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.formats.MediaView;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.UnifiedNativeAd;
+import com.google.android.gms.ads.formats.UnifiedNativeAdView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,17 +41,17 @@ public class NewsFeed extends AppCompatActivity {
 
     WebView webView;
 
-    @BindView(R.id.adView)
-    AdView mAdView;
-
     @BindView(R.id.progressBar)
     ProgressBar progressBar;
+
+    FirebaseRemoteConfig mFirebaseRemoteConfig;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feed);
         ButterKnife.bind(this);
+        MobileAds.initialize(this);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
@@ -44,18 +59,28 @@ public class NewsFeed extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_IMMERSIVE
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setMinimumFetchIntervalInSeconds(10) // change to 3600 on published app
+                .build();
+        mFirebaseRemoteConfig.setConfigSettingsAsync(configSettings);
+        mFirebaseRemoteConfig.fetchAndActivate().addOnCompleteListener(this, new OnCompleteListener<Boolean>() {
+            @Override
+            public void onComplete(@NonNull Task<Boolean> task) {
+                if (task.isSuccessful()) {
+                    if(mFirebaseRemoteConfig.getBoolean("startAd")) {
+                        initAdLoader();
+                    }
+                }
+            }
+        });
 
         Intent intent = getIntent();
         String newsUrl = intent.getStringExtra("newsUrl");
-
         progressBar.setMax(100);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
 
         webView = findViewById(R.id.wv);
-
         webView.loadUrl(newsUrl);
-
         webView.setWebViewClient(new client());
 
         WebSettings webSettings = webView.getSettings();
@@ -63,19 +88,39 @@ public class NewsFeed extends AppCompatActivity {
         webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         webView.clearCache(true);
         webView.clearHistory();
-        webView.setDownloadListener(new DownloadListener() {
-            @Override
-            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
-                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                dm.enqueue(request);
+        webView.setDownloadListener((url, userAgent, contentDisposition, mimetype, contentLength) -> {
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+            DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+            dm.enqueue(request);
 
-            }
         });
     }
 
-private class client extends WebViewClient
+    private void initAdLoader() {
+        AdLoader adLoader = new AdLoader.Builder(this, "ca-app-pub-3940256099942544/2247696110")
+                .forUnifiedNativeAd(unifiedNativeAd -> {
+                    UnifiedNativeAdView unifiedNativeAdView = (UnifiedNativeAdView) getLayoutInflater().inflate(R.layout.native_ad_layout, null);
+
+                    FrameLayout nativeAdLayout = findViewById(R.id.id_native_ad);
+                    nativeAdLayout.removeAllViews();
+                    mapUnifiedNativeAdToLayout(unifiedNativeAd, unifiedNativeAdView, nativeAdLayout);
+                    nativeAdLayout.addView(unifiedNativeAdView);
+                })
+                .withAdListener(new AdListener(){
+                    @Override
+                    public void onAdFailedToLoad(int i) {
+                        super.onAdFailedToLoad(i);
+                    }
+                })
+                .withNativeAdOptions(new NativeAdOptions.Builder()
+                        .build())
+                .build();
+
+        adLoader.loadAd(new AdRequest.Builder().build());
+    }
+
+    private class client extends WebViewClient
 {
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
@@ -95,4 +140,59 @@ private class client extends WebViewClient
         progressBar.setVisibility(View.GONE);
     }
 }
+    public void mapUnifiedNativeAdToLayout(UnifiedNativeAd adFromGoogle, UnifiedNativeAdView myAdView, FrameLayout nativeAdLayout) {
+        MediaView mediaView = myAdView.findViewById(R.id.ad_media);
+        myAdView.setMediaView(mediaView);
+
+        Button closeBtn = myAdView.findViewById(R.id.ad_call_to_action);
+        myAdView.setHeadlineView(myAdView.findViewById(R.id.ad_headline));
+        myAdView.setBodyView(myAdView.findViewById(R.id.ad_body));
+        myAdView.setIconView(myAdView.findViewById(R.id.ad_icon));
+        myAdView.setPriceView(myAdView.findViewById(R.id.ad_price));
+        myAdView.setStarRatingView(myAdView.findViewById(R.id.ad_rating));
+        myAdView.setStoreView(myAdView.findViewById(R.id.ad_store));
+        myAdView.setAdvertiserView(myAdView.findViewById(R.id.ad_advertiser));
+
+        ((TextView) myAdView.getHeadlineView()).setText(adFromGoogle.getHeadline());
+
+        if (adFromGoogle.getBody() == null) {
+            myAdView.getBodyView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getBodyView()).setText(adFromGoogle.getBody());
+        }
+
+       closeBtn.setOnClickListener(view -> nativeAdLayout.setVisibility(View.GONE));
+
+        if (adFromGoogle.getIcon() == null) {
+            myAdView.getIconView().setVisibility(View.GONE);
+        } else {
+            ((ImageView) myAdView.getIconView()).setImageDrawable(adFromGoogle.getIcon().getDrawable());
+        }
+
+        if (adFromGoogle.getPrice() == null) {
+            myAdView.getPriceView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getPriceView()).setText(adFromGoogle.getPrice());
+        }
+
+        if (adFromGoogle.getStarRating() == null) {
+            myAdView.getStarRatingView().setVisibility(View.GONE);
+        } else {
+            ((RatingBar) myAdView.getStarRatingView()).setRating(adFromGoogle.getStarRating().floatValue());
+        }
+
+        if (adFromGoogle.getStore() == null) {
+            myAdView.getStoreView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getStoreView()).setText(adFromGoogle.getStore());
+        }
+
+        if (adFromGoogle.getAdvertiser() == null) {
+            myAdView.getAdvertiserView().setVisibility(View.GONE);
+        } else {
+            ((TextView) myAdView.getAdvertiserView()).setText(adFromGoogle.getAdvertiser());
+        }
+
+        myAdView.setNativeAd(adFromGoogle);
+    }
 }
